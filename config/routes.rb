c7adb352dd10eb1d20a71b99d0c8d7c696a9a5ad
Jonathan Chang <jonoschang7@gmail.com>
2014-04-17 17:29:37 -0400

    Create log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Openseat::Application.routes.draw do
   resources :restaurants
   resources :users, :only => [:new, :create, :edit, :update, :show]
+  resources :sessions, :only => [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
