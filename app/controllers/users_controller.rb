3965f28169c31cb14ecc5499f4f62110d831e281
Jonathan Chang <jonoschang7@gmail.com>
2014-04-17 17:16:49 -0400

    Work on user controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to products_url, :notice => "Signed up!"
+		else
+			redirect_to new_user_url
+		end
 	end
 
 	def edit
@@ -12,5 +19,10 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
