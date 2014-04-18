8b7b0c674b8330e50fbf22d2903c453f357e8162
Jonathan Chang <jonoschang7@gmail.com>
2014-04-17 22:23:10 -0400

    Create reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417205118) do
+ActiveRecord::Schema.define(version: 20140418021309) do
+
+  create_table "reservations", force: true do |t|
+    t.string   "date"
+    t.string   "time"
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
